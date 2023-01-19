26ff8e6741096549ed9b9e196964ad658030a8f9
Carlos Augusto <caarlos.san@gmail.com>
2023-01-19 16:35:48 -0300

    Resolve req 4

---
+++ src/Archetypes/Archetypes.ts
@@ -0,0 +1,31 @@
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetype {
+  private readonly _name: string;
+  private readonly _special: number;
+  private readonly _cost: number;
+
+  constructor(name: string) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get special() {
+    return this._special;
+  }
+
+  get cost() {
+    return this._cost;
+  }
+
+  abstract get energyType(): EnergyType;
+}
